7af0352737306e2e975a1377c56a077491c018ae
yuritanpopo <riszp48dk@gmail.com>
2014-01-29 22:44:30 +0900

    Initial Commit

---
+++ gittest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gittest
+//
+//  Created by Makoto Mamiya on 2014/01/29.
+//  Copyright (c) 2014年 KLOON. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
